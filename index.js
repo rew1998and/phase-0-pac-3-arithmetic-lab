e905363d2681da18290804af49e8d4a0d8a790c3
rew1998and <149449325+rew1998and@users.noreply.github.com>
2023-12-16 12:35:14 -0800

    complete

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b;
+}
+let number = 10;
+function add5(a,b){
+    return (number += 5);
+}
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
